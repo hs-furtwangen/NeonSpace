b5963a32fb58cd13a933d6175146b4f9dc325e6f
Monika <monika.galkewitsch@hs-furtwangen.de>
2019-06-01 18:17:12 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Script/Asteroid.cs
+++ Assets/Script/Asteroid.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+enum size
+{
+    PLANET= 1000,
+    ASTEROID = 100,
+    DEBRIT = 50
+}
 public class Asteroid : MonoBehaviour
 {
-    static float mass_modifier = 100;
+    public size mass = size.ASTEROID;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        int mass_mod = mass;
+        float scale_multiplier = Random.Range(0.5f, 1.5f);
+        transform.localScale *= scale_multiplier;
         float size_mean = (transform.localScale.x + transform.localScale.y + transform.localScale.z) / 3;
-        rigidbody.mass = size_mean * mass_modifier;
+        rigidbody.mass = size_mean * mass_mod;
         this.enabled = false;
     }
 }
