7f47fd4c168661d00dcb48d765d384081c63c4d2
Monika <monika.galkewitsch@hs-furtwangen.de>
2019-06-01 14:14:39 +0200

    Better Physics

--- Assets/Script/Character_controller.cs
+++ Assets/Script/Character_controller.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Character_controller : MonoBehaviour
 {
     public Rigidbody rigidbody;
+    public Camera camera;
+    public Vector3 camera_offset;
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
     public float sensitivityX = 15F;
@@ -13,7 +15,8 @@ public class Character_controller : MonoBehaviour
     public float maximumX = 360F;
     public float minimumY = -360F;
     public float maximumY = 360F;
-    public float grabDistance = 250f;
+    public float maxDistance = 250f;
+    
     public float acceleration = 1;
     [SerializeField]
     float rotationX = 0F;
@@ -25,7 +28,10 @@ public class Character_controller : MonoBehaviour
     bool pull = false;
     [SerializeField]
     bool grab = false;
-    
+    [SerializeField]
+    private float holdDistance = 0f;
+    private GameObject camera_pivot;
+
     Vector3 invalidVector = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
     Vector3 target;
 
@@ -43,7 +49,6 @@ public class Character_controller : MonoBehaviour
         {
             if (grab)
             {
-                
                 grabToTarget(target);
             }
             else if (pull)
@@ -63,17 +68,18 @@ public class Character_controller : MonoBehaviour
                     }
                     else if (Input.GetMouseButton(1))
                     {
+                        holdDistance = Vector3.Distance(rigidbody.position, target);
                         grab = true;
                         pull = false;
                     }
                 }
             }
         }
-        else if (!Input.GetMouseButton(0))
+        if (!Input.GetMouseButton(0))
         {
             pull = false;
         }
-        else if (!Input.GetMouseButton(1))
+        if (!Input.GetMouseButton(1))
         {
             grab = false;
         }
@@ -87,6 +93,14 @@ public class Character_controller : MonoBehaviour
         Cursor.visible = false;
         // Make the rigid body not change rotation
         rigidbody = gameObject.GetComponent<Rigidbody>();
+        camera = gameObject.GetComponentInChildren<Camera>();
+        camera.transform.position = camera_offset;
+        camera_pivot = new GameObject();
+        camera_pivot.name = "Camera Pivot";
+        camera_pivot.transform.SetParent(this.gameObject.transform);
+        camera_pivot.transform.position = this.transform.position;
+        camera_pivot.transform.rotation = this.transform.rotation;
+        camera.gameObject.transform.SetParent(camera_pivot.transform);
         if (rigidbody)
             rigidbody.freezeRotation = true;
         originalRotation = transform.localRotation;
@@ -96,7 +110,7 @@ public class Character_controller : MonoBehaviour
     private Vector3 findTarget()
     {
         RaycastHit hit;
-        Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward), out hit, grabDistance);
+        Physics.Raycast(camera.transform.position, this.transform.TransformDirection(Vector3.forward), out hit, maxDistance);
         if (hit.collider != null)
         {
             
@@ -110,16 +124,35 @@ public class Character_controller : MonoBehaviour
 
     private void grabToTarget(Vector3 target)
     {
-        
-        Debug.DrawLine(this.transform.position, target, Color.red);
+        if(Vector3.Distance(this.rigidbody.position, target) < maxDistance)
+        { 
+            
+            Debug.DrawRay(this.transform.position, (target - this.rigidbody.position), Color.yellow, 1);
+            if (Vector3.Distance(this.rigidbody.position, target) >= holdDistance)
+            {
+                Vector3 oldPos = rigidbody.position;
+                Vector3 newPos = Vector3.Lerp(rigidbody.position, (target - Vector3.ClampMagnitude((target - this.rigidbody.position), holdDistance)), 1);
+                rigidbody.position = newPos;
+                Quaternion rotation = Quaternion.FromToRotation(oldPos, newPos*2);
+                Debug.DrawRay(rigidbody.position, rigidbody.velocity * maxDistance, Color.red, 1);
+                //rigidbody.velocity = Vector3.RotateTowards(rigidbody.velocity, newPos, angle, 1);
+                rigidbody.AddForce(-rigidbody.velocity * Time.deltaTime);
+                rigidbody.velocity = rotation * rigidbody.velocity;
+                Debug.DrawRay(rigidbody.position, rigidbody.velocity * maxDistance, Color.green, 1);
+            }
+        }
+
 
     }
 
     private void pullToTarget(Vector3 target)
     {
-        Debug.DrawRay(this.transform.position, Vector3.MoveTowards(this.transform.position, target, 250));
-        Debug.Log(target);
-        rigidbody.AddForce(target - this.rigidbody.position);
+        Debug.DrawRay(this.transform.position, (target - this.rigidbody.position),Color.blue,1);
+        if(Vector3.Distance(this.rigidbody.position, target)<= maxDistance)
+        {
+            rigidbody.AddForce((target - this.rigidbody.position)*(acceleration*Time.deltaTime));
+        }
+        
     }
 
     private void rotatePlayer()
