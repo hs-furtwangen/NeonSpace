6dc8cab339aaf2b1819c365746f9c115b2e8c096
Elke <elke.scherffius@hs-furtwangen.de>
2019-06-02 04:09:44 +0200

    Startmenu hinzugefügt

---
+++ Assets/Script/ButtonScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+	public List<GameObject> canvasPanels = new List<GameObject>();
+	int howToState = 0;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void StartButton(){
+		//Starte Spiel
+	}
+	
+	public void HowToButton(){
+		canvasPanels[0].SetActive(false);
+		canvasPanels[2].SetActive(true);
+		howToState = 0;
+	}
+	
+	public void ForwardButton(){
+		howToState++;
+		
+		canvasPanels[2 + howToState -1].SetActive(false);
+		canvasPanels[2 + howToState].SetActive(true);
+	}
+	
+	public void BackButton(){
+		howToState--;
+		
+		canvasPanels[2 + howToState +1].SetActive(false);
+		canvasPanels[2 + howToState].SetActive(true);
+	}
+	
+	public void MainmenuButton(){
+		//Laden der Startscene wenn Spieler Spiel verlassen will
+	}
+	
+	public void CreditsButton(){
+		canvasPanels[0].SetActive(false);
+		canvasPanels[1].SetActive(true);
+	}
+	
+	public void CloseButton(){
+		canvasPanels[0].SetActive(true);
+		
+		for(int i = 1; i<canvasPanels.Count; i++){
+			canvasPanels[i].SetActive(false);
+		}
+	}
+	
+	public void QuitButton(){
+		//Schließe Spiel
+	}
+}
---
+++ Assets/Script/WollschweberStartmenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WollschweberStartmenu : MonoBehaviour {
+	Vector3 mouse = new Vector3();
+	Vector3 mouseWorld = new Vector3();
+	Vector3 mouseForward = new Vector3();
+	Vector2 faceDirection = new Vector2();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouse = Input.mousePosition;
+		mouseWorld = Camera.main.ScreenToWorldPoint(mouse);
+		
+		faceDirection = new Vector2(mouseWorld.x - transform.position.x, mouseWorld.y - transform.position.y);
+		//mouseForward = mouseWorld + transform.position;
+		transform.up = faceDirection;
+		//transform.rotation = Quaternion.LookRotation(mouseForward, Vector3.forward);
+	}
+}
